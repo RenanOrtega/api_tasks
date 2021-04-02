3f4c7b1a2886497cddb9626fdb0fa6c5f948f5ab
VictorRibeiro13 <victorribeiro.docs.13@gmail.com>
2021-04-02 17:07:33 -0300

    refactor(migration): change relation between task and situation

--- src/database/migrations/20210329223633-relationBetweenTaskAndSituation.js
+++ src/database/migrations/20210329223633-relationBetweenTaskAndSituation.js
@@ -13,22 +13,9 @@ module.exports = {
       onDelete: "SET NULL",
       onUpdate: "CASCADE",
     });
-
-    await queryInterface.addColumn("situations", "task_id", {
-      type: Sequelize.INTEGER,
-      references: {
-        model: {
-          tableName: "tasks",
-        },
-        key: "id",
-      },
-      onDelete: "SET NULL",
-      onUpdate: "CASCADE",
-    });
   },
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.removeColumn("tasks", "situation_id");
-    await queryInterface.removeColumn("situations", "task_id");
   },
 };
